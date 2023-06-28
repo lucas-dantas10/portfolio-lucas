e934127b0ad1391d76aa44ae1fce7717e5bd5242
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-06-28 12:39:56 -0300

    Acionando o AOS e o botao de retornar ao topo global

--- src/main.js
+++ src/main.js
@@ -1,20 +1,27 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './css/style.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Loading from 'vue-loading-overlay';
+import AOS from 'aos'
+import BackToTop from 'vue-backtotop';
+
 import 'vue-loading-overlay/dist/css/index.css';
-
-library.add([
-    faGithub, faLinkedin, faInstagram
-]);
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import 'aos/dist/aos.css'
+import './css/style.css';
+
+library.add([
+    faGithub, faLinkedin, faInstagram, faArrowUp
+]);
+
+AOS.init();
 
 createApp(App)
+.use(BackToTop)
 .component('font-awesome-icon', FontAwesomeIcon)
 .component('loading', Loading)
 .mount('#app');

689e1bb45d6d2e726cd8bf61ed4e28e7a990670b
Lucas Dantas <lucas.dantas.nogueira@gmail.com>
2023-07-04 17:49:31 -0300

    Botao para subir ao clicar nele

---
+++ src/components/Button/BtnGoTop.vue
@@ -0,0 +1,19 @@
+<template>
+    <back-to-top class="btn-style">
+        <font-awesome-icon class="icons" :icon="{ prefix: 'fas', iconName: 'arrow-up' }"/>
+    </back-to-top>
+</template>
+
+<style scoped>
+
+.btn-style {
+    border-radius: 30px;
+    border: 1px solid var(--violet);
+    background-color: var(--violet);
+    padding: 0.5rem 1rem;
+}
+.icons {
+    font-size: 30px;
+    color: var(--white);
+}
+</style>
